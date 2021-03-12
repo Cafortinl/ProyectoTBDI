ac5e38214197db8b7ccdc8404954aea80169e9e1
Carlos Fortín <cafortinl@hotmail.com>
2021-03-11 18:59:21 -0600

    DBAdmin v1.0

---
+++ ProyectoTDBI_Grupo4/DBAdmin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Npgsql;
+
+namespace ProyectoTDBI_Grupo4
+{
+    class DB
+    {
+        private NpgsqlConnection conn;
+        private NpgsqlCommand cmd;
+        private NpgsqlDataReader dr;
+        public DB(string connString)
+        {
+            conn = new NpgsqlConnection(connString);
+        }
+
+        public void open()
+        {
+            conn.Open();
+        }
+        public void close()
+        {
+            conn.Close();
+        }
+        public void defineQuery(string query)
+        {
+            cmd = new NpgsqlCommand(query, conn);
+        }
+
+        public NpgsqlDataReader executeQuery()
+        {
+            return dr = cmd.ExecuteReader();
+        }
+        
+    }
+}
