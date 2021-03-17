c8ee8725659328db9957664da0c3991b743039dd
MarcelaRivera2000 <marci.eunicer@gmail.com>
2021-03-16 22:32:31 -0600

    call center

--- ProyectoTDBI_Grupo4/CallCenter.xaml.cs
+++ ProyectoTDBI_Grupo4/CallCenter.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace ProyectoTDBI_Grupo4
 {
@@ -20,6 +21,50 @@ namespace ProyectoTDBI_Grupo4
         public CallCenter()
         {
             InitializeComponent();
+        }
+
+        private void Cll_BtCompra_Click(object sender, RoutedEventArgs e)
+        {
+            int idCliente = Convert.ToInt32(Cll_idCliente.Text);
+            int  nOrden = Convert.ToInt32(Cll_nOrden.Text);
+            int nSeguimiento = Convert.ToInt32(Cll_nSeguimiento.Text);
+            string nombreRemitente = Cll_nRemitente.Text;
+            string empresaEnvio = Cll_EmpresaEnvio.Text;
+            string DireccionEnvio = Cll_DirreccionEnvio.Text;
+            string Host = "proyectotdbi.ce6kih4lqvgw.us-east-1.rds.amazonaws.com";
+            string User = "administrador";
+            string DBname = "ProyectoTDBI";
+            string Port = "5432";
+            string Password = "AdM1n_Pr0yect0#1";
+            string connString =
+            String.Format(
+                    "Server={0};Port={1};Database={2};User Id={3};Password={4};",
+                    Host,
+                    Port,
+                    DBname,
+                    User,
+                    Password);
+
+            DBAdmin dba = new DBAdmin(connString);
+            try
+            {
+                dba.open();
+                dba.defineQuery("SELECT * FROM Clientes");
+                NpgsqlDataReader dr = dba.executeQuery();
+                if (dr.HasRows)
+                {
+                    
+                }
+                else
+                    MessageBox.Show("El usuario ingresado no se encuentra en la base de datos. Intentelo de nuevo.");
+                dba.close();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+                throw;
+            }
+
         }
     }
 }
