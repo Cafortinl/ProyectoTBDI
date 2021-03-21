74ca0e189b0364eb781990a6b8837bd48ad72aeb
Memo-espinal <guille.gaes@gmail.com>
2021-03-20 21:09:40 -0600

    Bodega Doing

--- ProyectoTDBI_Grupo4/Bodega.xaml.cs
+++ ProyectoTDBI_Grupo4/Bodega.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,9 +18,59 @@ namespace ProyectoTDBI_Grupo4
     /// </summary>
     public partial class Bodega : Window
     {
+        private static string Host = "proyectotdbi.ce6kih4lqvgw.us-east-1.rds.amazonaws.com";
+        private static string User = "administrador";
+        private static string DBname = "ProyectoTDBI";
+        private static string Port = "5432";
+        private static string Password = "AdM1n_Pr0yect0#1";
+        private static string connString =
+        String.Format(
+                "Server={0};Port={1};Database={2};User Id={3};Password={4};",
+                Host,
+                Port,
+                DBname,
+                User,
+                Password);
+        private DBAdmin dba = new DBAdmin(connString);
+        //private static string user;
         public Bodega()
         {
             InitializeComponent();
+            //user = u;
+            NpgsqlDataReader dr;
+            dba.open();
+            List<String> infoTiend = new List<String>();
+            dba.defineQuery("SELECT * FROM tienda");
+            dr = dba.executeQuery();
+            while (dr.Read())
+            {
+                infoTiend.Add(Convert.ToString(dr.GetInt32(0)) + " , " + dr.GetString(1));
+            }
+            CB_Bodega.ItemsSource = infoTiend;
+            dba.close();
+        }
+
+        private void CB_Bodega_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            tablaProducto();
+        }
+
+        private void tablaProducto()
+        {
+            NpgsqlDataReader dr;
+            dba.open();
+            string idd = (Convert.ToString(CB_Bodega.SelectedItem)).Split(",")[0];
+            List<Producto> listaOrden = new List<Producto>();
+            dba.defineQuery("SELECT * FROM producto NATURAL JOIN inventario WHERE \"codigoTienda\" = " + idd);
+            dr = dba.executeQuery();
+            while (dr.Read())
+            {
+                listaOrden.Add(new Producto(dr.GetString(1), dr.GetInt32(0), dr.GetString(2), dr.GetString(3), dr.GetString(4), dr.GetString(5), dr.GetDouble(6)));
+            }
+            DG_Bodega.ItemsSource = listaOrden;
+            DG_Bodega.CanUserAddRows = false;
+            dba.close();
+
         }
     }
 }
