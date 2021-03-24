481ea810721e571695c479452566cc455fe1a397
Carlos Fortín <cafortinl@hotmail.com>
2021-03-24 14:26:31 -0600

    :v v5 creo xd

---
+++ ProyectoTDBI_Grupo4/Registrarse.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoTDBI_Grupo4
+{
+    /// <summary>
+    /// Interaction logic for Registrarse.xaml
+    /// </summary>
+    public partial class Registrarse : Window
+    {
+        public Registrarse()
+        {
+            InitializeComponent();
+        }
+    }
+}
